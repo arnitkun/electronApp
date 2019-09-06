ab90314903629784fe479ae26512663fb4440a55
arnitkun <ankit.nitagartala@gmail.com>
2019-09-06 11:19:10 +0530

    electronApp-07 added polygon annotation drawing functionality

--- app/index.js
+++ app/index.js
@@ -1,1 +1,8 @@
+var image = require(lightning-image-poly);
 var picture = require('cat-picture');
+
+var src = picture.src;
+picture.remove();
+
+var viz = new image('#visualization', null, [src], {hullAlgorithm: 'convex'});
+
