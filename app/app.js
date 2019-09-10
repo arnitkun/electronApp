c8584ab29473ed90d27ead0b1ba72461ee8348e2
arnitkun <ankit.nitagartala@gmail.com>
2019-09-10 14:56:24 +0530

    electronApp-07 Set webpreferences to true, require won't work without it

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 var electron = require('electron')  
-       
+   
     electron.app.on('ready', function () {  
-       var mainWindow = new electron.BrowserWindow({width: 600, height: 800});  
+       var mainWindow = new electron.BrowserWindow({width: 800, height: 600, webPreferences: {nodeIntegration: true}});  
        mainWindow.loadURL('file://' + __dirname + '/index.html');  
+       
     });
